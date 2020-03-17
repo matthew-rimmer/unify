a1d77787df0e4cf1601e5729d8db9cc7b0fb36fd
Rich0000 <46540800+richardkhill@users.noreply.github.com>
2020-03-17 13:47:50 +0000

    Update unifyapp.py

--- unify/unifyapp.py
+++ unify/unifyapp.py
@@ -69,7 +69,7 @@ class LoginPayload(object):
 
 
 	
-# THE POST REQUEST DOESN'T CURRENTLY WORK!
+##### ----------------- THE POST REQUEST DOESN'T CURRENTLY WORK! ------------------ #####
 	
 #Sign up payload layout
 #j = {"User_ID":"Temp123","Email":"temp@gmail.com","First_Name":"Temp","Last_Name":"Chair","DateOfBirth":"2000-05-27","Password":"Temppassword123","Profile_Picture":null,"Twitter_Link":"https://twitter.com/temp/","Instagram_Link":"https://www.instagram.com/temp/","Description":"Temp likes Computer Science","User_Created":"2020-02-20T14:02:32Z","Last_Login":"2020-02-27T17:40:32Z"}
